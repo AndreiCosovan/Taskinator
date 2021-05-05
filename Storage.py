091faad69f24f665d86805f30e618814a9eaa3d8
Andrei Cosovan <andreicosovan@web.de>
2021-05-05 12:49:54 +0200

    Datepicker edited/Storage editted

--- Storage.py
+++ Storage.py
@@ -1,12 +1,12 @@
 import json
 
-class Storage:
-    def save(self, data):
-        with open("to_dos.json", "w") as to_dos:
-            json.dump(data, to_dos)
+
+def save(self, data):
+    with open("to_dos.json", "w") as to_dos:
+        json.dump(data, to_dos)
 
 
-    def update(self):
-        with open("to_dos.json", "r") as to_dos:
-            data = json.load(to_dos)
-            return data 
+def update(self):
+    with open("to_dos.json", "r") as to_dos:
+        data = json.load(to_dos)
+        return data 
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from WindowManager import WindowManager
 from screens import MainWindow
+from kivymd.uix.picker import MDDatePicker
 
 #kv = Builder.load_file("to_do_.kv")
 sm = WindowManager()
@@ -27,6 +28,16 @@ class to_do_App(MDApp):
     def build(self):
         self.root = Builder.load_file('to_do_.kv')
         self.theme_cls.theme_style = "Dark"
+
+    def show_date_picker(self):
+        date_dialog = MDDatePicker(callback=self.on_save)
+        #date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
+        date_dialog.open()
+
+    def on_save(self, date):
+        print(date)
+
+
 
 
 
